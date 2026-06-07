048f518616080fb0325e7af73ba3c8390471e731
atif2112011 <atif251171@gmail.com>
2026-06-07 18:10:20 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(helmet());
 
 app.use(cors({
-  origin: "*",
+  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : ['http://localhost:3000', 'http://localhost:3001'],
   credentials: true,
 }));
 
